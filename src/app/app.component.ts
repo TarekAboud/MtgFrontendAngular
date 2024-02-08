960aa8863555324c1a1669e11f6ebda180130935
tarek Ajax <tarekjoud2@gmail.com>
2024-02-08 13:27:49 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from "@angular/common/http"
+import { DxSelectBoxModule, DxListModule, 
+  DxTemplateModule } from 'devextreme-angular';
+import {  } from 'devextreme-angular'
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, RouterOutlet, HttpClientModule, 
+    DxSelectBoxModule, DxListModule, DxTemplateModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'MagicTheGatheringLotrFrontend';
+  readonly ApiUrl = "http://localhost:2766/api/cards/" 
+  sets: any=[];
+  setCards: any=[];
+
+  constructor(private _http: HttpClient) {
+  }
+
+
+  refreshSets() {
+    this._http.get(this.ApiUrl + 'GetAllSets')
+    .subscribe( {
+      next: (d) => {this.sets = d},
+      error: (e) => console.error(e) 
+    })
+  }
+
+  onSetClick(e: any) {
+    console.log(e);
+    this.getAllSetCards(e.itemData.code);
+  }
+
+  getAllSetCards(setId: Text) {
+    this._http.get(this.ApiUrl + 'GetSetCards?setId=' + setId)
+    .subscribe( {
+      next: (d) => {this.setCards = d},
+      error: (e) => console.error(e) 
+    })
+  }
+
+  ngOnInit() {
+    this.refreshSets();
+  }
+
 }
